265a5b5983079ac0f6862f849aebcd24fb8148e1
Rezonans <87856097+rezocrypt@users.noreply.github.com>
2025-01-31 14:55:31 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const ResoDB = require('./libs/resodb');
-const cors = require('cors')
 
 const app = express();
 const port = 3000; // Replace with your desired port number
@@ -16,7 +15,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Parse JSON bodies (as sent by API clients)
 app.use(bodyParser.json());
-app.use(cors())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'get.html')); // Send a response back
